04453d905df361f6205b1278906d1e2e2b9267a0
RansomD19 <dan2007gigi@gmail.com>
2022-06-09 15:01:32 +0300

    hellothere

--- website/js/gallery.js
+++ website/js/gallery.js
@@ -22,7 +22,29 @@ if (galleryImages) {
             let newImg = document.createElement("img");
             newImgWindow.appendChild(newImg);
             newImg.setAttribute("src", "website/img/" + setNewImgUrl);
+            newImg.setAttribute("id", "current-img");
 
+            newImg.onload = function () {
+                let imgWidth = this.width;
+                let calImgToEdge = ((windowWidth - imgWidth) / 2) - 100;
+
+                let newNextBtn = document.createElement('a');
+                let btnNextText = document.createTextNode('Next');
+                newNextBtn.appendChild(btnNextText);
+                container.appendChild(newNextBtn);
+                newNextBtn.setAttribute("class", "img-btn-next");
+                newNextBtn.setAttribute("onclick", "changeImg(1)");
+                newNextBtn.style.cssText = "right: " + calImgToEdge + "px;";
+
+                let newPrevBtn = document.createElement('a');
+                let btnPrevText = document.createTextNode('Prev');
+                newPrevBtn.appendChild(btnPrevText);
+                container.appendChild(newPrevBtn);
+                newPrevBtn.setAttribute("class", "img-btn-prev");
+                newPrevBtn.setAttribute("onclick", "changeImg(0)");
+                newPrevBtn.style.cssText = "left: " + calImgToEdge + "px;";
+
+            }
 
         }
     });
@@ -30,4 +52,47 @@ if (galleryImages) {
 
 function closeImg() {
     document.querySelector(".img-window").remove();
+    document.querySelector(".img-btn-next").remove();
+    document.querySelector(".img-btn-prev").remove();
+}
+
+function changeImg(changeDir) {
+    document.querySelector("#current-img").remove();
+    let getImgWindow = document.querySelector(".img-window");
+    let newImg = document.createElement('img');
+    getImgWindow.appendChild(newImg);
+
+    let calcNewImg;
+    if (changeDir === 1) {
+        calcNewImg = getLatestOpenedImg + 1;
+        if (calcNewImg > galleryImages.length) {
+            calcNewImg = 1;
+
+        }
+    }
+
+    else if (changeDir === 0) {
+        calcNewImg = getLatestOpenedImg - 1;
+        if (calcNewImg < 1) {
+            calcNewImg = galleryImages.length;
+        }
+    }
+
+
+    newImg.setAttribute("src", "website/img/img" + calcNewImg + ".jpeg")
+    newImg.setAttribute("id", "current-img")
+
+    getLatestOpenedImg = calcNewImg;
+
+    newImg.onload = function () {
+        let imgWidth = this.width;
+        let calImgToEdge = ((windowWidth - imgWidth) / 2) - 100;
+
+        let nextBtn = document.querySelector(".img-btn-next");
+        nextBtn.style.cssText = "right: " + calImgToEdge + "px;";
+
+        let prevBtn = document.querySelector(".img-btn-prev");
+        prevBtn.style.cssText = "left: " + calImgToEdge + "px;";
+
+    }
 }
